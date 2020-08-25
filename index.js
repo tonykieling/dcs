7301fcba1dd5cef490793acc297f087b6e39db1d
Tony Kieling <tony.kieling@gmail.com>
2020-08-24 19:09:46 -0700

    trying a way of tracking sms message

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
 // const express     = require("express");
-// const PORT        = process.env.PORT || 3456;
+const PORT        = process.env.PORT || 3456;
 // const path        = require('path');
 // const app         = express();
 // const morgan      = require("morgan");
 // const bodyParser  = require("body-parser");
+
 const mongoose    = require("mongoose");
 require('dotenv').config();
 
-// const userRoutes      = require("./api/routes/user.js");
-// const clientRoutes    = require("./api/routes/client.js");
-// const clockinRoutes   = require("./api/routes/clockin.js");
-// const invoiceRoutes   = require("./api/routes/invoice.js");
-console.log("test")
+try {
+  mongoose.connect(process.env.DB, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(z => console.log(" => MongoDB connection success!"))
+    .catch(error => console.log("error:", error.message));
+
+} catch (err) {
+  console.log("error on MongoDB connection: ", err);
+}
 
 
-// const connection = () => {
-  try {
-    mongoose.connect(process.env.DB, { 
-      useNewUrlParser: true,
-      useUnifiedTopology: true })
-      .then(e => console.log(e))
+const express = require('express');
+const session = require('express-session');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-    // console.log("conn=> ", conn.then());
-  } catch (err) {
-    console.log("error on MongoDB connection");
-    console.log(err.message);
+const app = express();
+
+app.use(session({secret: 'anything-you-want-but-keep-secret'}));
+
+app.post('/sms', (req, res) => {
+  console.log("req==>", req);
+
+  const smsCount = req.session.counter || 0;
+
+  let message = 'Hello, thanks for the new message.';
+
+  if(smsCount > 0) {
+    message = 'Hello, thanks for message number ' + (smsCount + 1);
   }
-// }
 
-// console.log(connection())
+  req.session.counter = smsCount + 1;
+
+  const twiml = new MessagingResponse();
+  twiml.message(message);
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`);
+});
