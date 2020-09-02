cdf6134c4f86ee598a3da12f4fc32c03c0bf3e60
Tony Kieling <tony.kieling@gmail.com>
2020-09-01 20:12:46 -0700

    small changes/fixes on conversation.js

--- message/conversation.js
+++ message/conversation.js
@@ -18,14 +18,16 @@ router.post("/", async(req, res) => {
 
   const response = new MessagingResponse();
 
+
+  // if there is no cookie for the sender
   if (!conversationHistory) {
-    const from = req.body.From; // thius is phone number which is sending the message
+    const from = req.body.From; // this is phone number which is sending the message
     const twilio              = require('twilio'),
           TWILIO_ACCOUNT_SID  = process.env.TWILIO_ACCOUNT_SID,
           TWILIO_AUTH_TOKEN   = process.env.TWILIO_AUTH_TOKEN;
     const client              = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-    // let's grab info avout the last message sent to this phone number and
+    // let's grab info about the last message sent to this phone number and
     // figure out what stage is the conversation
     // it should be the first reply message after receiving from our system OR
     // an answer for the above, but after Twilio cookie has expired - 4 hours (from Twilio documentation)
@@ -38,13 +40,15 @@ router.post("/", async(req, res) => {
         // dateSentBefore: new Date(Date.UTC(2020, 8, 30, 0, 0, 0)),
       });
 
-    console.log("NUmber of messages:", historyMessages.length);
-    console.log("======================================\n historyMessages", historyMessages);
+    // console.log("NUmber of messages:", historyMessages.length);
+    // console.log("======================================\n historyMessages", historyMessages);
 
-    if ((historyMessages[0].body.search("behalf") !== -1) || (historyMessages[0].body.search("can you send Yes"))) {
+    if ((historyMessages[0].body.search("behalf") !== -1)) {
       conversationHistory = 1;
-    } else if (historyMessages[0].body.search("Today") !== -1){
+    } else if (historyMessages[0].body.search("Credit Card")){
       conversationHistory = 2;
+    } else if ((historyMessages[0].body.search("Today") !== -1)) {
+      conversationHistory = 3;
     } else {
       response.message(`\nSorry. Conversation was lost.\nPlease call 123-456-7890 for further information.`);
       return res.send(response.toString());
@@ -59,16 +63,16 @@ router.post("/", async(req, res) => {
       if ((body.toLowerCase() === "yes") 
           || (body.toLowerCase() === "y") 
           || (body.toLowerCase() === "yep")
+          || (body.toLowerCase() === "yeah")
           || (body.toLowerCase() === "sure")) {
-        response.message(`\nGreat! \nI am assuming you are able to pay all the debt.\nDo you wanna pay by?\n Send\n  1 for Credit Card or \n  2 for Transferwise.`);
+        response.message(`\nGreat! \nWe are assuming you are able to pay all the debt once.\nDo you wanna pay by?\n Send\n  1 for Credit Card or \n  2 for Transferwise.`);
         cachedConversation = 2;
       } else if ((body.toLowerCase() === "no") 
           || (body.toLowerCase() === "nope") 
+          || (body.toLowerCase() === "n") 
           || (body.toLowerCase() === "not really")) {
         response.message(`\nThank you for your honest answer.\nAny information, please call 123.`);
         res.clearCookie("conversation");
-        res.type("text/xml");
-        return res.send(response.toString());        
       } else {
         response.message(`\nI cannot understand what you mean.\n Please, can you send Yes if you are able to pay or No if not?`);
         cachedConversation = 1;
@@ -79,10 +83,10 @@ router.post("/", async(req, res) => {
     case 2:
       console.log(" --- Inside CASE 22");
       if ((Number(body) === 1) || (Number(body) === 2)) {
-        response.message(`\Awesome! \nPlease type \n1 for Today or \n2 for Tomorrow.`);
+        response.message(`\Awesome! \n Please type \n  1 for Today or \n  2 for Tomorrow.`);
         cachedConversation = 3;
       } else {
-        response.message(`\nI cannot understand what you mean.\n Please, \nDo you wanna pay by?\n Send\n  1 for Credit Card or \n  2 for Transferwise.`);
+        response.message(`\nWe cannot understand what you mean.\n Please, \nDo you wanna pay by?\n Send\n  1 for Credit Card or \n  2 for Transferwise.`);
         cachedConversation = 2;
       }
       res.cookie("conversation", cachedConversation, { maxAge: cookieTimeAlive });
@@ -96,7 +100,7 @@ router.post("/", async(req, res) => {
         res.type("text/xml");
         return res.send(response.toString());
       } else {
-        response.message(`\nPlease type \n1 for Today or \n2 for Tomorrow.`);
+        response.message(`\nPlease type \n  1 for Today or \n  2 for Tomorrow.`);
         cachedConversation = 3;
       }
       res.cookie("conversation", cachedConversation, { maxAge: cookieTimeAlive });
